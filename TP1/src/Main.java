7bdff13d1af2d7040a10d3a001c3c7d702a4593b
optimasfides <optimaa-fide@mail.ru>
2020-09-23 11:45:41 -0400

    test 2

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
 
         // write your code here
     }
